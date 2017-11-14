7645f23bbb77a88a401f4a84efc1d565ac7e0159
gichehak <gichehak@udel.edu>
2017-11-14 11:52:19 -0500

    with demo

--- songbase.py
+++ songbase.py
@@ -1,18 +1,72 @@
-from flask import Flask
+from flask import Flask, session, render_template, request, flash, redirect, url_for
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to get secure key'
 
 
 @app.route('/')
 def index():
-        return '<h1>hello world!!!</h1>'
+    # return HTML
+    # return "<h1>this is the index page!<h1>"
+    return render_template('index.html')
 
-@app.route('/user/<string:name>/')
-def get_user(name):
-    return 'hello %s your age is %d' % (name, 3)
+
+@app.route('/songs')
+def show_all_songs():
+    songs = [
+        'Paradise',
+        'Yellow',
+        'Viva La Vida'
+    ]
+
+    return render_template('song-all.html', songs=songs)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 @app.route('/users')
 def show_all_users():
-    return '<h2>This is the page for all users</h2>'
+    return render_template('user-all.html')
 
+
+@app.route('/form-demo', methods=['GET', 'POST'])
+def form_demo():
+    # how to get form data is different for GET vs. POST
+    if request.method == 'GET':
+        first_name = request.args.get('first_name')
+        if first_name:
+            return render_template('form-demo.html', first_name=first_name)
+        else:
+            return render_template('form-demo.html', first_name=session.get('first_name'))
+    if request.method == 'POST':
+        session['first_name'] = request.form['first_name']
+        # return render_template('form-demo.html', first_name=first_name)
+        return redirect(url_for('form_demo'))
+
+
+@app.route('/user/<string:name>/')
+def get_user_name(name):
+    # return "hello " + name
+    # return "Hello %s, this is %s" % (name, 'administrator')
+    return render_template('user.html', name=name)
+
+
+@app.route('/song/<int:id>/')
+def get_song_id(id):
+    # return "This song's ID is " + str(id)
+    return "Hi, this is %s and the song's id is %d" % ('administrator', id)
+
+
+# https://goo.gl/Pc39w8 explains the following line
 if __name__ == '__main__':
-    app.run(host ='0.0.0.0', port= 2000)
+
+    # activates the debugger and the reloader during development
+    # app.run(debug=True)
+    app.run()
+
+    # make the server publicly available on port 80
+    # note that Ports below 1024 can be opened only by root
+    # you need to use sudo for the following conmmand
+    # app.run(host='0.0.0.0', port=80)
